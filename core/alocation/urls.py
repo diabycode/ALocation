df5ed2253db373b97b547d016961852923d2ed3d
Diaby Almamy <diabyissouf149@gmail.com>
2023-08-21 15:48:39 +0000

    Edits in alocation app ! Features added :    - new view : not_staff_user    - redirect not staff users    - generic template added : generic-page.html    - stylesheet, css reset file : reset.css

--- core/alocation/urls.py
+++ core/alocation/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from .views import AppLoginView, make_payment, app, logout_user
+from .views import *
 from . import settings
 
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('login/', AppLoginView.as_view(), name='login'),
     path('logout-user/', logout_user, name='logout-user'),
     path('make-payment/', make_payment, name='make_payment'),
+    path('not-staff-user/', not_staff_user, name='not-staff-user'),
     
     path('renters/', include('renter.urls')),
     path('locals/', include('local.urls')),
--- core/alocation/views.py
+++ core/alocation/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.shortcuts import redirect
 from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import reverse
 
 from .forms import CustomAuthenticationForm
@@ -20,11 +20,19 @@ class AppLoginView(LoginView):
         return reverse("renter:renters-list")
 
 
+@user_passes_test(lambda u: u.is_authenticated and u.is_staff, redirect_field_name="not-staff-user")
 @login_required(redirect_field_name="login")
 def app(request):
     return redirect("renter:renters-list")
 
 
+@user_passes_test(lambda u: u.is_authenticated and not u.is_staff, redirect_field_name="not-staff-user")
+@login_required()
+def not_staff_user(request):
+    return render(request, "alocation/not-staff-user.html")
+
+
+@user_passes_test(lambda u: u.is_authenticated and u.is_staff, redirect_field_name="not-staff-user")
 @login_required(redirect_field_name="login")
 def make_payment(request):
     if request.method != "POST":
